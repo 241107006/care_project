e9541f1b3d29401d4edee2ff6c82fb0ce67f0ac2
Almas <almasbackend@livin.kz>
2025-04-26 02:01:46 +0500

    a

--- care_project/settings.py
+++ care_project/settings.py
@@ -130,17 +130,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/var/www/zabotaplus.kz/static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/zabotaplus.kz/media'
 
 # Additional static files settings
 STATICFILES_FINDERS = [
